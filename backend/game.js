770dd845f5881140ba0c4297dfaebe9f0cee6752
Salik47 <msalik32@gmail.com>
2023-01-06 18:45:33 +0530

    Added commments and express server

--- backend/game.js
+++ backend/game.js
@@ -1,11 +1,13 @@
 const { GRID_SIZE } = require("./constants");
 
+// Initialize the game sandbox
 function initializeGame() {
   const state = createGameState();
   generateFoodRandomly(state);
   return state;
 }
 
+// Creating the initial values of both the players and size of the grid and storing them in an object
 function createGameState() {
   return {
     players: [
@@ -50,15 +52,19 @@ function gameLoop(state) {
     return;
   }
 
+  // Getting the first and second player from the state object
   const playerOne = state.players[0];
   const playerTwo = state.players[1];
 
+  // Updating the position of the player one
   playerOne.position.x += playerOne.velocity.x;
   playerOne.position.y += playerOne.velocity.y;
 
+  // Updating the position of the player two
   playerTwo.position.x += playerTwo.velocity.x;
   playerTwo.position.y += playerTwo.velocity.y;
 
+  // Checking if the player one is out of the grid
   if (
     playerOne.position.x < 0 ||
     playerOne.position.x > GRID_SIZE ||
@@ -68,6 +74,7 @@ function gameLoop(state) {
     return 2;
   }
 
+  // Checking if the player two is out of the grid
   if (
     playerTwo.position.x < 0 ||
     playerTwo.position.x > GRID_SIZE ||
@@ -77,6 +84,7 @@ function gameLoop(state) {
     return 1;
   }
 
+  // Checking if the player one is eating the food and updating the position of the player one and generating the food randomly
   if (
     state.food.x === playerOne.position.x &&
     state.food.y === playerOne.position.y
@@ -87,6 +95,7 @@ function gameLoop(state) {
     generateFoodRandomly(state);
   }
 
+  // Checking if the player two is eating the food and updating the position of the player two and generating the food randomly
   if (
     state.food.x === playerTwo.position.x &&
     state.food.y === playerTwo.position.y
@@ -99,22 +108,26 @@ function gameLoop(state) {
 
   if (playerOne.velocity.x || playerOne.velocity.y) {
     for (let cell of playerOne.snake) {
+      // Checking if the player one is bumping into itself and returning the player two as the winner
       if (cell.x === playerOne.position.x && cell.y === playerOne.position.y) {
         return 2;
       }
     }
 
+    // Else increasing the length of the snake
     playerOne.snake.push({ ...playerOne.position });
     playerOne.snake.shift();
   }
 
   if (playerTwo.velocity.x || playerTwo.velocity.y) {
     for (let cell of playerTwo.snake) {
+      // Checking if the player two is bumping into itself and returning the player one as the winner
       if (cell.x === playerTwo.position.x && cell.y === playerTwo.position.y) {
         return 1;
       }
     }
 
+    // Else increasing the length of the snake
     playerTwo.snake.push({ ...playerTwo.position });
     playerTwo.snake.shift();
   }
@@ -128,21 +141,24 @@ function generateFoodRandomly(state) {
     y: Math.floor(Math.random() * GRID_SIZE),
   };
 
+  // Checking if the food is not on the snake of the player one, if it is then generating the food randomly again
   for (let cell of state.players[0].snake) {
     if (cell.x === food.x && cell.y === food.y) {
       return generateFoodRandomly(state);
     }
   }
 
+  // Checking if the food is not on the snake of the player two, if it is then generating the food randomly again
   for (let cell of state.players[1].snake) {
     if (cell.x === food.x && cell.y === food.y) {
       return generateFoodRandomly(state);
     }
   }
 
-  state.food = food;
+  state.food = food; // Storing the food in the state object
 }
 
+// Updating the velocity of the player based on the key pressed, i.e., changing the direction of the snake
 function getUpdatedVelocity(keyCode) {
   switch (keyCode) {
     case 37: {
